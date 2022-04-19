abe724ea25de59b5ea903fdd3c9433c6488f5849
mounika <mounikaaileni7@gmail.com>
2022-04-19 23:10:24 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import React ,{componet} from 'react';
+//import Product from "./pages/Product";
+// import ProductList from "./pages/ProductList";
+// import Register from "./pages/Register";
+// import Login from "./pages/Login";
+// import Cart from "./pages/Cart"
+const App = () => {
+  return <Cart/>;
+};
 
 export default App;
